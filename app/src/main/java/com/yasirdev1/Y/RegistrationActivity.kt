8e7109f526f7c122f011b9358525ae95ac78bdd3
yasir-dev1 <112517455+yasir-dev1@users.noreply.github.com>
2024-02-10 18:23:51 +0300

    Auth with Firebase

---
+++ app/src/main/java/com/yasirdev1/Y/RegistrationActivity.kt
@@ -0,0 +1,100 @@
+package com.yasirdev1.Y
+
+import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+
+
+class RegistrationActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    @SuppressLint("WrongViewCast")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+
+
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+        var CreateAccount = findViewById<TextView>(R.id.CreateAccount)
+        var Login = findViewById<TextView>(R.id.Login)
+        var Email = findViewById<EditText>(R.id.Email)
+        var Password = findViewById<EditText>(R.id.Password)
+
+
+        CreateAccount.setOnClickListener {
+            val  Emailtext = Email.text.toString()
+            val EmailRegx = Regex("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}\$")
+            if (Emailtext.isEmpty()){
+                Toast.makeText(this, "Please Enter a Email", Toast.LENGTH_SHORT).show()
+            }else if (!EmailRegx.matches(Emailtext)) {
+                Toast.makeText(this, "Please Enter vaild  Email", Toast.LENGTH_SHORT).show()
+            }else{
+                auth.createUserWithEmailAndPassword(Emailtext, Password.text.toString())
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success")
+                            val user = auth.currentUser
+                            Toast.makeText(
+                                baseContext,
+                                "User Created successfully ",
+                                Toast.LENGTH_SHORT,
+                            ).show()
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                            Toast.makeText(
+                                baseContext,
+                                task.exception?.message.toString(),
+                                Toast.LENGTH_SHORT,
+                            ).show()
+
+                        }
+                    }
+            }
+        }
+
+        Login.setOnClickListener {
+            val  Emailtext = Email.text.toString()
+            val EmailRegx = Regex("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}\$")
+            if (Emailtext.isEmpty()){
+                Toast.makeText(this, "Please Enter a Email", Toast.LENGTH_SHORT).show()
+            }else if (!EmailRegx.matches(Emailtext)) {
+                Toast.makeText(this, "Please Enter vaild  Email", Toast.LENGTH_SHORT).show()
+            }else{
+                auth.signInWithEmailAndPassword(Emailtext, Password.text.toString())
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success")
+                            val user = auth.currentUser
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.exception)
+                            Toast.makeText(
+                                baseContext,
+                                task.exception?.message.toString(),
+                                Toast.LENGTH_SHORT,
+                            ).show()
+
+                        }
+                    }
+            }
+        }
+    }
+
+
+
+}
